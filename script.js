fe8d55dc01113cd7544f6220ac5d26c04c1bdcb4
Eduardoo1233 <160926458+Eduardoo1233@users.noreply.github.com>
2026-03-06 15:21:37 +0000

    Crud finalizado

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
 let tarefas = []
 
 const inputTarefa = document.getElementById('inputTarefa')
+const btnEnviar = document.getElementById('submitTarefa')
+const lista = document.getElementById('lista')
+
+function adicionarTarefa () {
+    tarefas.push(inputTarefa.value)
+}
+
+function mostrarTarefa () {
+    lista.innerHTML = tarefas.map((tarefa, index) => `
+        <li>
+        <span>${tarefa}</span>
+        <div class="acoes">
+        <button class="edit" onclick="editTarefa(${index})">Editar</button>
+        <button class="delete" onclick="excluirTarefa(${index})">Deletar</button>
+        </li>
+        </li>`)
+        .join("")
+}
+
+function limparInput () {
+    inputTarefa.value = ""
+}
+
+function editTarefa (index) {
+    const novaTarefa = prompt("Editar Tarefa:", tarefas[index])
+    if (novaTarefa !== null) {
+        tarefas[index] = novaTarefa
+        mostrarTarefa()
+    }
+}
+
+function excluirTarefa (index) {
+    tarefas.splice(index, 1)
+    mostrarTarefa()
+}
 
 // EVENTOS
-document.getElementById('submitTarefa').addEventListener('click', function() {
-    tarefas.push(inputTarefa.value)
-    console.log(tarefas)
+
+btnEnviar.addEventListener('click', () => {
+    adicionarTarefa()
+    mostrarTarefa()
+    limparInput()
 })
