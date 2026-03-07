35b84b685566f728f021b6206455bc84809ae736
Eduardoo1233 <160926458+Eduardoo1233@users.noreply.github.com>
2026-03-07 02:25:04 +0000

    Adicionando local Storage e checkbox

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-let tarefas = []
+let tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
 
 const inputTarefa = document.getElementById('inputTarefa')
 const btnEnviar = document.getElementById('submitTarefa')
 const lista = document.getElementById('lista')
 
+function limparInput () {
+    inputTarefa.value = ""
+}
+
 function adicionarTarefa () {
-    tarefas.push(inputTarefa.value)
+    if (inputTarefa.value.trim() == "") {
+        alert("Você não pode enviar uma tarefa vazia!")
+    } else {
+        tarefas.push(inputTarefa.value)
+        salvarTarefas()
+    }
+}
+
+function editTarefa (index) {
+    const novaTarefa = prompt("Editar Tarefa:", tarefas[index])
+    if (novaTarefa !== null) {
+        tarefas[index] = novaTarefa
+        salvarTarefas()
+        mostrarTarefa()
+    }
+}
+
+function excluirTarefa (index) {
+    tarefas.splice(index, 1)
+    salvarTarefas()
+    mostrarTarefa()
 }
 
 function mostrarTarefa () {
     lista.innerHTML = tarefas.map((tarefa, index) => `
         <li>
+        <input type="checkbox">
         <span>${tarefa}</span>
         <div class="acoes">
         <button class="edit" onclick="editTarefa(${index})">Editar</button>
@@ -18,24 +43,12 @@ function mostrarTarefa () {
         </li>
         </li>`)
         .join("")
+}        
+
+function salvarTarefas() {
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
 }
 
-function limparInput () {
-    inputTarefa.value = ""
-}
-
-function editTarefa (index) {
-    const novaTarefa = prompt("Editar Tarefa:", tarefas[index])
-    if (novaTarefa !== null) {
-        tarefas[index] = novaTarefa
-        mostrarTarefa()
-    }
-}
-
-function excluirTarefa (index) {
-    tarefas.splice(index, 1)
-    mostrarTarefa()
-}
 
 // EVENTOS
 
@@ -44,3 +57,5 @@ btnEnviar.addEventListener('click', () => {
     mostrarTarefa()
     limparInput()
 })
+
+mostrarTarefa()
